421df23fdd7a86bc6db7384d646ee2ab2ce5fb92
Tho <xuantho573@gmail.com>
2024-06-13 22:16:10 +0700

    add c++ implementation for built-in sort solution

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/built-in-sort/built-in-sort.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int minMovesToSeat(vector<int> &seats, vector<int> &students) {
+    sort(seats.begin(), seats.end());
+    sort(students.begin(), students.end());
+    int res = 0;
+    for (int i = 0; i < seats.size(); i++)
+      res += abs(seats[i] - students[i]);
+    return res;
+  }
+};
